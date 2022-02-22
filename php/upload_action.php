fe90517a7458e1f446158c8313c7c6400eca4934
Yasunori-Park <99373791+Yasunori-Park@users.noreply.github.com>
2022-02-23 01:02:37 +1100

    Create upload_action.php

---
+++ php/upload_action.php
@@ -0,0 +1,1 @@
+<meta http-equiv="refresh" content="2;URL=http://scigendetection.imag.fr/TPD52/Va/upload_e0dced36ed77e57fa93ced9bb9a08421/advance_info.phphttp://scigendetection.imag.fr/TPD52/Va/upload_e0dced36ed77e57fa93ced9bb9a08421/advance_info.php"><p> Action started </p><p> Upload failed </p><p> Action Over </p>
